ccc3faafe3f7a63dcb7c2e114974b40dbc099e01
Jesse Tomchak <jtomchak@gmail.com>
2018-02-28 15:22:50 -0700

    basic auth endpoint with passport

---
+++ models/User.js
@@ -0,0 +1,38 @@
+var mongoose = require("mongoose");
+var uniqueValidator = require("mongoose-unique-validator");
+var crypto = require("crypto");
+var jwt = require("jsonwebtoken");
+var secret = require("../config").secret;
+
+var UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: [true, "can't be blank"],
+      match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+      index: true
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: [true, "can't be blank"],
+      match: [/\S+@\S+\.\S+/, "is invalid"],
+      index: true
+    },
+    bio: String,
+    image: String,
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Article" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    hash: String,
+    salt: String
+  },
+  {
+    timestamps: true,
+    usePushEach: true
+  }
+);
+
+mongoose.model("User", UserSchema);
