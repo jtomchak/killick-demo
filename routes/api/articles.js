444849ec6619ab970258529342b62af33a36767e
Jesse Tomchak <jtomchak@gmail.com>
2018-03-07 11:44:54 -0700

    article routing API

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -2,6 +2,22 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 const Article = mongoose.model("Article");
 
+// Preload article objects on routes with ':article'
+router.param("article", function(req, res, next, slug) {
+  Article.findOne({ slug: slug })
+    .populate("author")
+    .then(function(article) {
+      if (!article) {
+        return res.sendStatus(404);
+      }
+
+      req.article = article;
+
+      return next();
+    })
+    .catch(next);
+});
+
 router.get("/", function(req, res, next) {
   //this is where we'd query db, give all the articles
   // get the results
@@ -14,6 +30,48 @@ router.get("/", function(req, res, next) {
       return res.json({
         articles: results
       });
+    })
+    .catch(next);
+});
+
+// return a article by slug-id
+router.get("/:article", auth.optional, function(req, res, next) {
+  Promise.all([
+    req.payload ? User.findById(req.payload.id) : null,
+    req.article.populate("author").execPopulate()
+  ])
+    .then(function(results) {
+      var user = results[0];
+
+      return res.json({ article: req.article.toJSONFor(user) });
+    })
+    .catch(next);
+});
+
+// return an article's comments
+router.get("/:article/comments", auth.optional, function(req, res, next) {
+  Promise.resolve(req.payload ? User.findById(req.payload.id) : null)
+    .then(function(user) {
+      return req.article
+        .populate({
+          path: "comments",
+          populate: {
+            path: "author"
+          },
+          options: {
+            sort: {
+              createdAt: "desc"
+            }
+          }
+        })
+        .execPopulate()
+        .then(function(article) {
+          return res.json({
+            comments: req.article.comments.map(function(comment) {
+              return comment.toJSONFor(user);
+            })
+          });
+        });
     })
     .catch(next);
 });
